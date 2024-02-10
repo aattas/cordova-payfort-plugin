775535bf7fe366355b829671f1f5aede68225aba
André Grillo <andregrillo@gmail.com>
2024-02-10 01:03:09 +0000

    Checkout Implemented

--- src/ios/Payfort.swift
+++ src/ios/Payfort.swift
@@ -8,9 +8,10 @@ import Foundation
 import CryptoKit
 import PayFortSDK
 
-@objc
+@objc(Payfort)
 class Payfort: CDVPlugin {
     var command: CDVInvokedUrlCommand?
+    var payFort: PayFortController!
     
     @objc(initialize:)
     func initialize(_ command: CDVInvokedUrlCommand){
@@ -23,12 +24,11 @@ class Payfort: CDVPlugin {
     
     @objc(getUdid:)
     func getUdid(_ command: CDVInvokedUrlCommand){
-        var payFort: PayFortController!
         
         if let environment = command.arguments[0] as? String {
-            if environment == "production" {
+            if environment == "1" {
                 payFort = PayFortController(enviroment: .production)
-            } else if environment == "development" {
+            } else if environment == "2" {
                 payFort = PayFortController(enviroment: .sandBox)
             } else {
                 sendPluginResult(callbackId: command.callbackId, status: CDVCommandStatus_ERROR, message: "Invalid arguments")
@@ -81,6 +81,49 @@ class Payfort: CDVPlugin {
             sendPluginResult(callbackId: command.callbackId, status: CDVCommandStatus_OK, message: hash.map { String(format: "%02x", $0) }.joined())
         } else {
             sendPluginResult(callbackId: command.callbackId, status: CDVCommandStatus_ERROR, message: "Error generating signature")
+        }
+    }
+    
+    @objc(checkout:)
+    func checkout(_ cdvcommand: CDVInvokedUrlCommand){
+        
+        if let amount = cdvcommand.arguments[0] as? String,
+           let command = cdvcommand.arguments[1] as? String,
+           let currency = cdvcommand.arguments[2] as? String,
+           let customer_email = cdvcommand.arguments[3] as? String,
+           let installments = cdvcommand.arguments[4] as? String,
+           let language = cdvcommand.arguments[5] as? String,
+           let sdk_token = cdvcommand.arguments[6] as? String {
+            
+           let request = ["amount" : amount,
+               "command" : command,
+               "currency" : currency,
+               "customer_email" : customer_email,
+               "installments" : installments,
+               "language" : language,
+               "sdk_token" : sdk_token]
+            
+            //        let request = ["amount" : "1000",
+            //            "command" : "AUTHORIZATION",
+            //            "currency" : "AED",
+            //            "customer_email" : "rzghebrah@payfort.com",
+            //            "installments" : "",
+            //            "language" : "en",
+            //            "sdk_token" : "token"]
+            
+            payFort.callPayFort(withRequest: request, currentViewController: self.viewController, success: { (requestDic, responseDic) in
+                        print("✅ success")
+                self.sendPluginResult(callbackId: cdvcommand.callbackId, status: CDVCommandStatus_OK)
+                    },
+                    canceled: { (requestDic, responseDic) in
+                        print("🚨 canceled")
+                self.sendPluginResult(callbackId: cdvcommand.callbackId, status: CDVCommandStatus_ERROR, message: "Cancelled")
+                    },
+                    faild: { (requestDic, responseDic, message) in
+                        print("🚨 failed")
+                self.sendPluginResult(callbackId: cdvcommand.callbackId, status: CDVCommandStatus_ERROR, message: "Failed")
+            })
+            
         }
     }
 }
--- www/Payfort.js
+++ www/Payfort.js
@@ -7,3 +7,7 @@ exports.generateSignature = function (json, pass, success, error) {
 exports.getUdid = function (environment, success, error) {
     exec(success, error, 'Payfort', 'getUdid', [environment]);
 };
+
+exports.checkout = function (amount, command, currency, customer_email, installments, language, sdk_token, success, error) {
+    exec(success, error, 'Payfort', 'checkout', [amount,command,currency,customer_email,installments,language,sdk_token]);
+};
